dfb23dc8c3f73172390295f20808f0f6c6b67cdc
Quynh To Tuan <quynh.totuan@gmail.com>
2017-04-26 01:22:17 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,16 @@
-def hello_t
-
+def hello_t(array)
+  i = 0
+  while i < array.length do
+    yield array[i]
+    i += 1
+  end
+  array
 end
 
 # call your method here!
-
+names = ["Tim", "Tom", "Jim"]
+hello_t(names) do |e|
+  if e.start_with?("T")
+    puts "Hi, #{e}"
+  end
+end
